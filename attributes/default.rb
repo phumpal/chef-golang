5b9c7f559d2c6ca75d8cf3559117bfecb0e8a07d
Matthew Moore <matt@crowdmob.com>
2013-06-25 12:02:51 -0700

    1.1.1 go upgrade and 32 bit checking

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default["go"]["version"] = "1.0.3"
-default["go"]["platform"] = "amd64"
+default["go"]["version"] = "1.1.1"
+default["go"]["platform"] = kernel['machine'] =~ /x86_64/ ? "amd64" : "386"
 default["go"]["filename"] = "go#{node["go"]["version"]}.#{node["os"]}-#{node["go"]["platform"]}.tar.gz"
 default['go']['url'] = "http://go.googlecode.com/files/#{node["go"]["filename"]}"
 default['go']['install_dir'] = "/usr/local"
