73109d0ad39f7b2f0bf90ec48bd472af83820bab
phumpal <patrick@netvilla.net>
2014-03-10 13:06:51 -0700

    Go version update

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["go"]["version"] = "1.1.1"
+default["go"]["version"] = "1.2.1"
 default["go"]["platform"] = kernel['machine'] =~ /x86_64/ ? "amd64" : "386"
 default["go"]["filename"] = "go#{node["go"]["version"]}.#{node["os"]}-#{node["go"]["platform"]}.tar.gz"
 default['go']['url'] = "http://go.googlecode.com/files/#{node["go"]["filename"]}"
